84c48507dfab5b031c25726e885a10fb3ab62c68
Robby Grossman <robby@freerobby.com>
2012-05-03 11:52:44 -0400

    Add dependency on 'install_from' cookbook as required to use install_from_release.

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,7 @@ description      "Installs/Configures torquebox"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
+depends "install_from"
 depends "java"
 
 supports "ubuntu"
