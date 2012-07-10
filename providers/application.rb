4150abb6d252da4f1a4a92d4fa3968fed892fb3a
Robby Grossman <robby@freerobby.com>
2012-07-10 15:57:49 -0400

    Deploy via knob file.

--- providers/application.rb
+++ providers/application.rb
@@ -6,13 +6,17 @@ def initialize(*args)
 end
 
 action :deploy do
-  execute "torquebox deploy #{new_resource.path}" do
-    creates "#{deployments}/#{new_resource.name}-knob.yml.deployed"
+  execute "torquebox archive #{new_resource.path}" do
+    cwd "#{new_resource.path}/../../"
+  end
+  execute "torquebox deploy #{new_resource.path}.knob" do
+    creates "#{deployments}/#{new_resource.name}.knob-knob.yml.deployed"
   end
 end
 
 action :undeploy do
   execute "torquebox undeploy #{new_resource.path}" do
-    creates "#{deployments}/#{new_resource.name}-knob.yml.undeployed"
+    creates "#{deployments}/#{new_resource.name}.knob-knob.yml.undeployed"
   end
 end
+                                                                                
