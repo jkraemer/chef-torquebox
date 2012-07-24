adaf3eb8c49a2f918fd9736679e0044135a7ec95
Robby Grossman <robby@freerobby.com>
2012-07-24 10:56:57 -0400

    Run post-file-system deploy commands outside of the timestamped_deploy LWRP.

--- providers/rails_app.rb
+++ providers/rails_app.rb
@@ -11,11 +11,6 @@ action :deploy do
     group "torquebox"
   end
   
-  # Need to get/set these variables because new_resource won't be available once we're in a different lwrp (timestamped_deploy)
-  app_configuration = new_resource.configuration
-  app_name = new_resource.name
-  app_install_in = new_resource.install_in
-  
   timestamped_deploy "#{new_resource.install_in}/#{new_resource.name}" do
     scm_provider Chef::Provider::Git
     repo new_resource.git_repository
@@ -27,46 +22,8 @@ action :deploy do
     shallow_clone true
     
     environment "RACK_ENV" => "production", "JRUBY_OPTS" => node[:torquebox][:jruby][:opts]
-    
-    before_migrate do
-      execute "jruby -S gem install bundler" do
-        cwd release_path
-      end
-      execute "jruby -J-Xmx2048m -J-Xms512m -J-Xmn128m -S bundle install --without development test --deployment" do
-        cwd release_path
-      end
-    end
-    
-    migrate true
-    migration_command "jruby -S bundle exec rake db:migrate"
-    
-    # This should probably be in a pre_symlink_to_current_directory callback, but chef gives us none.
-    # It's safe because our torquebox deploy runs later anyway, but it would be problematic if we were
-    # hosting live files from the new release before its assets were compiled.
-    before_restart do
-      execute "compile assets" do
-        user "torquebox"
-        group "torquebox"
-        cwd release_path
-        command "jruby -J-Xmx2048m -J-Xms512m -J-Xmn128m -S bundle exec rake assets:precompile"
-        environment "RACK_ENV" => "production", "JRUBY_OPTS" => node[:torquebox][:jruby][:opts]
-      end
-    end
-    
-    restart_command do
-      # Construct/clobber the YAML file
-      require "yaml"
-      file "#{release_path}/config/torquebox.yml" do
-        content app_configuration.to_yaml
-      end
 
-      # Deploy to Torquebox
-      torquebox_application "tb_app:#{app_name}" do
-        action :deploy
-        path "#{app_install_in}/#{app_name}/current"
-      end
-    end
-    
+    migrate false
     purge_before_symlink %w{}
     create_dirs_before_symlink %w{}
     symlinks Hash.new # {} doesn't work, as it gets parsed as a block
@@ -75,9 +32,65 @@ action :deploy do
     action :deploy
   end
   
-  # This should probably go in the restart command, but chef does not provide a way to access a top level provider's
-  # attributes while nested within a second provider. Since restart runs last anyway, it's easy to just do it here.
+  # By standard Chef practices, the following pieces would be executed within the timestamped_deploy LWRP. I've done them
+  # afterwords for two reasons:
+  # 1. We make use of nested resources, and as of Chef 10.12, there is no way for nested resources to access their
+  #    parents' attributes.
+  # 2. Jruby commands within the timestamped_deploy resource were failing when run via "knife ssh", which runs a ptty
+  #    session rather than a tty session. Even when I set the environment variables manually I was getting errors
+  #    finding gems.
+  # Because torquebox manages its deploys independently of the Chef timestamped_deploy LWRP, it is easier to do all of
+  # this work after the file system is deployed, and then hand it off to Chef. -RG 07/24/2012
   
+  app_environment = {
+    "RACK_ENV" => "production",
+    "JRUBY_OPTS" => node[:torquebox][:jruby][:opts]
+  }
+  app_directory = "#{new_resource.install_in}/#{new_resource.name}/current"
+  
+  # Install bundler
+  execute "jruby -S gem install bundler" do
+    user "torquebox"
+    group "torquebox"
+    cwd app_directory
+    environment app_environment
+  end
+  
+  # Vendor the gems
+  execute "jruby -S bundle install --without development test --deployment" do
+    user "torquebox"
+    group "torquebox"
+    cwd app_directory
+    environment app_environment
+  end
+  
+  # Run database migrations
+  execute "jruby -S bundle exec rake db:migrate" do
+    user "torquebox"
+    group "torquebox"
+    cwd app_directory
+    environment app_environment
+  end
+  
+  # Precompile asset pipeline assets
+  execute "jruby -J-Xmx2048m -J-Xms512m -J-Xmn128m -S bundle exec rake assets:precompile" do
+    user "torquebox"
+    group "torquebox"
+    cwd app_directory
+    environment app_environment
+  end
+  
+  # Construct/clobber the YAML file
+  require "yaml"
+  file "#{app_directory}/config/torquebox.yml" do
+    content new_resource.configuration.to_yaml
+  end
+  
+  # Deploy to Torquebox
+  torquebox_application "tb_app:#{new_resource.name}" do
+    action :deploy
+    path app_directory
+  end
   
 end
 
